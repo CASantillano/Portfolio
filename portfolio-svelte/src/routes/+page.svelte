38fd248fee0c3d57cc4aa2fa5a9d0693ca44c5c9
Cassandra Santillano <casantillano@csu.fullerton.edu>
2026-03-07 15:32:34 -0800

    Replace old portfolio with svelte structure

---
+++ portfolio-svelte/src/routes/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+</script>
+
+<h1>Welcome to My Portfolio</h1>
+<p>work in progress</p>
+
+<a href="/projects" class="nav-link">Projects</a>
+
+<style>
+  h1 {
+    text-align: center;
+    margin-top: 2rem;
+    background: pink;
+  }
+
+  p {
+    text-align: center;
+    color: black;
+  }
+
+  a {
+    display: block;
+    text-align: center;
+    margin-top: 1rem;
+    color: rgb(9, 7, 7);
+  }
+</style>
