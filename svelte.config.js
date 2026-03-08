847b4fa8509eb6d4e04614d777b7f3b314f878ff
Cassandra Santillano <casantillano@csu.fullerton.edu>
2026-03-07 16:55:42 -0800

    fixed base path for github pages

--- svelte.config.js
+++ svelte.config.js
@@ -10,7 +10,14 @@ const config = {
 			pages: 'docs',
 			assets: 'docs',
 			fallback: 'index.html'
-		})
+		}),
+
+		paths: {
+			base: process.env.NODE_ENV === 'production'
+			? '/Portfolio'
+			: ''
+    }
+
 	}
 };
 
